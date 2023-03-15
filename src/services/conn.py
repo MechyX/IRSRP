dce2acca4bb1651e6b2b15ce1aeb2217ba67dae2
Mechy <rit6569@gmail.com>
2023-03-15 23:00:37 +0530

    Use env var instead of hardcoding

--- src/services/conn.py
+++ src/services/conn.py
@@ -1,12 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from logger import get_logger
 from pymongo import MongoClient
 
 logger = get_logger(__name__)
 
 
+load_dotenv()
+
+
 def get_database():
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb+srv://mechy:subhamechyir@irsrp-cluster-0.onx2rrm.mongodb.net/?retryWrites=true&w=majority"
+    CONNECTION_STRING = os.getenv("MONGO_CONNECTION_URL")
+
     client = MongoClient(CONNECTION_STRING)
 
     create_collection(client.irsrp)
